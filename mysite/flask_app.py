7cce796ae879c4b7aa4b193ff75107e87798fb2f
NicholasSeward <nicholas.seward@gmail.com>
2023-08-23 08:48:04 -0500

    Added a basic website with 3 pages

---
+++ mysite/flask_app.py
@@ -0,0 +1,23 @@
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+    
+@app.route('/bio')
+def bio():
+    return send_from_directory(directory=app.static_folder, path='bio.html')
+@app.route('/links')
+def links():
+    return send_from_directory(directory=app.static_folder, path='links.html')
+@app.route('/schedule')
+def schedule():
+    return send_from_directory(directory=app.static_folder, path='schedule.html')
+
+if __name__=="__main__":
+	app.run(host='0.0.0.0', port=80)
